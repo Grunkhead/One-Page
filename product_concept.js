f26b2634d1ceeacf182e74282e606379d7b7e3f7
Dave <Grunkhead@users.noreply.github.com>
2018-09-27 18:40:22 +0200

    Added sections and a Game. Did some other minor tweaks.

---
+++ product_concept.js
@@ -0,0 +1,181 @@
+// In the form is a button waiting to be clicked.
+// When it is clicked it sets the cookie and saves the values locally.
+
+  // Seperate function for fetching the form data.
+  function getFormData() {
+
+    var formData = [];
+
+    formData['name']    = document.getElementById("name").value;
+    formData['email']   = document.getElementById("email").value;
+    formData['message'] = document.getElementById("message").value;
+
+    return formData;
+  }
+
+  function setCookie() {
+
+      // Create a new date timestamp.
+      var date = new Date();
+      date.setTime( date.getTime() + ( 365 * 24 * 60 * 60 * 1000 ));
+
+      // Fetch the form data.
+      const formData = getFormData();
+
+      const expires = 'expires=' + date.toUTCString();
+      const name    = 'name='    + formData['name'];
+      const email   = 'email='   + formData['email'];
+      const message = 'message=' + formData['message'];
+
+      // Set the form cookie
+      const myCookie = document.cookie =
+      'formCookie' + "=" +
+      name         + ";" +
+      email        + ";" +
+      message      + ";" +
+      expires      + ";";
+
+      // Log it for some caveman debugging. Check console for cookie data.
+      console.log(myCookie);
+}
+
+// GAME LOGIC under here.
+
+function generateGameLayout() {
+
+  var gameSection = document.getElementById('game-section');
+  var board       = document.createElement('ul');
+  board.className = 'game-board';
+
+  for (var i = 1; i <= 9; i++) {
+
+    var node = document.createElement('li');
+    node.addEventListener('click', function(event) {
+      makeTurn(event.target);
+    });
+
+    node.id  = i;
+
+    board.appendChild(node);
+  }
+
+  gameSection.appendChild(board);
+}
+
+var isPlayerOne = true;
+var playedTurns = 0;
+
+function makeTurn(node) {
+
+  // Check if field is empty to make a move.
+  if (!fieldEmpty(node)) {
+    return;
+  }
+
+  if (isPlayerOne) {
+
+    setCross(node);
+    isPlayerOne = false;
+    playedTurns++;
+
+  } else if(!isPlayerOne) {
+
+    setCircle(node);
+    isPlayerOne = true;
+    playedTurns++;
+  }
+
+  checkForWinner();
+}
+
+// Check if max turns are played, then reset the board.
+function gameEnded() {
+  return playedTurns < 9 ? false : true;
+}
+
+function checkForWinner() {
+
+  // Possible ways how a player could win.
+  const winningDataSets = [
+    // Horizontal
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+
+    // Vertical
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['3', '6', '9'],
+
+    // Diagonal
+    ['1', '5', '9'],
+    ['3', '5', '7']
+  ]
+
+  // Get claimed fields of both players.
+  const playerOneNodes = Array.from(document.getElementsByClassName('crossed'));
+  const playerTwoNodes = Array.from(document.getElementsByClassName('circled'));
+
+  winningDataSets.forEach((dataSet) => {
+
+    // Points will be automatically set to 0 when iteration is over.
+    var playerOnePoints = 0;
+    var playerTwoPoints = 0;
+
+    // Count the points of both players.
+    playerOnePoints = countPlayerPoints(playerOneNodes, dataSet, playerOnePoints);
+    playerTwoPoints = countPlayerPoints(playerTwoNodes, dataSet, playerTwoPoints);
+
+    // Check if any player has claimed a winning streak!
+    checkForWinningPlayer(playerOnePoints, 'yellow');
+    checkForWinningPlayer(playerTwoPoints, 'orange');
+  });
+
+  if (gameEnded()) {
+    alert('Nobody won the game ..');
+    newGame();
+  }
+}
+
+function checkForWinningPlayer(playerPoints, winningColor) {
+  if (playerPoints >= 3) {
+    alert(`The ${winningColor} player wins the game!`);
+    newGame();
+  }
+}
+
+function countPlayerPoints(nodes, dataSet, playerPoints) {
+  nodes.forEach((node) => {
+    if (dataSet.includes(node.id)) playerPoints++;
+  });
+
+  return playerPoints;
+}
+
+function fieldEmpty(node, state) {
+  return node.className == '' ? true : false;
+}
+
+function setCross(node) {
+  node.className = 'crossed';
+}
+
+function setCircle(node) {
+  node.className = 'circled';
+}
+
+function newGame() {
+
+  // Reset game values.
+  isPlayerOne = true;
+  playedTurns = 0;
+
+  // Take first item out of the HTMLCollection and remove it so it wipes the board.
+  document.getElementsByClassName('game-board')[0].remove();
+
+  // Generate a new fresh board.
+  generateGameLayout();
+}
+
+// Generate game elements so the game can be played.
+generateGameLayout();
